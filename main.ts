b40a41f9c2b92ee8c657477a633a8e6d02dfe874
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2026-01-09 13:25:06 -0500

    trash box code

--- main.ts
+++ main.ts
@@ -1,8 +1,46 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2026 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Sultan
+ * Created on: Jan 2026
+ * This program moves a servo when something is within 10cm of a sonar
 */
+// Variables
+const servoNumber1 = robotbit.Servos.S1
+let distanceToObject = 0
+let servoMoved = false
 
-basic.showString('Hello, World!')
+// Setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// Continuously check distance
+basic.forever(function () {
+
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    // Only if the distance is valid (greater than 0)
+    if (distanceToObject > 0) {
+
+        // If object is close and servo has not moved yet
+        if (distanceToObject < 10) {
+            if (servoMoved == false) {
+                robotbit.Servo(servoNumber1, 160)
+                basic.showIcon(IconNames.No)
+                servoMoved = true
+            }
+        } else {
+            // Reset servo when object moves away
+            if (servoMoved == true) {
+                robotbit.Servo(servoNumber1, 0)
+                basic.showIcon(IconNames.Happy)
+                servoMoved = false
+            }
+        }
+    }
+
+    basic.pause(500)
+})
