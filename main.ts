7dc1b52c8ab89e2d246f676aa7505914ce929c58
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2026-01-19 13:31:48 -0500

    fix

--- main.ts
+++ main.ts
@@ -4,16 +4,18 @@
  * Created on: Jan 2026
  * This program moves a servo when something is within 10cm of a sonar
 */
-// Variables
+
+
+// variables
 const servoNumber1 = robotbit.Servos.S1
 let distanceToObject = 0
 let servoMoved = false
 
-// Setup
+// setup
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-// Continuously check distance
+// continuously check distance
 basic.forever(function () {
 
     distanceToObject = sonar.ping(
@@ -22,18 +24,18 @@ basic.forever(function () {
         PingUnit.Centimeters
     )
 
-    // Only if the distance is valid (greater than 0)
+    // only if the distance is greater than 0
     if (distanceToObject > 0) {
 
         // If object is close and servo has not moved yet
         if (distanceToObject < 10) {
             if (servoMoved == false) {
-                robotbit.Servo(servoNumber1, 160)
+                robotbit.Servo(servoNumber1, 150)
                 basic.showIcon(IconNames.No)
                 servoMoved = true
             }
         } else {
-            // Reset servo when object moves away
+            // reset servo when object moves away
             if (servoMoved == true) {
                 robotbit.Servo(servoNumber1, 0)
                 basic.showIcon(IconNames.Happy)
@@ -42,5 +44,5 @@ basic.forever(function () {
         }
     }
 
-    basic.pause(500)
+    basic.pause(1000)
 })
